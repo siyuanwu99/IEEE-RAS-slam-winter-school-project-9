059ca084fc3c4ad797a062790ce26552fd7c1b32
edmundwsy <edmundwsy@outlook.com>
2021-07-09 00:43:25 +0800

    finish data augmentation

--- src/train.py
+++ src/train.py
@@ -1,17 +1,12 @@
 import numpy as np
 import tensorflow as tf
 import argparse
-from PIL import Image  # Imports PIL module
-from io import BytesIO
-from tensorflow.keras.utils import Sequence
-from skimage.transform import resize
-from tensorflow.keras.applications import DenseNet169
-import sklearn
-import os
+import cv2
 import matplotlib.pyplot as plt
 import time
 from loss import *
 from model import *
+import data_augmentation as transform
 
 from skimage import io
 from zipfile import ZipFile
@@ -32,24 +27,45 @@ parser.add_argument('--lr', type=int, default=0.0001, help='Learning rate')
 
 args = parser.parse_args()  # Add input as parameters
 
+## Data augmentation !!!
+# optional: rotate15, rotate60, rotate45, vertical_flip, horizontal_flip, random_crop, affine_transfrom
+data_augmentation = transform.rotate60
 
-def _parse_function(filename, label):
-    # Read images from disk
-    shape_rgb = (240, 320, 3)
-    shape_depth = (240, 320, 1)
-    image_decoded = tf.image.decode_jpeg(tf.io.read_file(filename))
-    depth_resized = tf.image.resize(tf.image.decode_jpeg(tf.io.read_file(label)),
-                                    [shape_depth[0], shape_depth[1]])
-
-    ## Data augmentation
-
-    # Format
-    rgb = tf.image.convert_image_dtype(image_decoded, dtype=tf.float32)
-    depth = tf.image.convert_image_dtype(depth_resized / 255.0, dtype=tf.float32)
-
-    # Normalize the depth values (in cm)
-    depth = 1000 / tf.clip_by_value(depth * 1000, 10, 1000)
-    return rgb, depth
+# def _parse_function(filename, label):
+#     # Read images from disk
+#
+#     image_decoded = tf.image.decode_jpeg(tf.io.read_file(filename))
+#     depth_decoded = tf.image.decode_jpeg(tf.io.read_file(label))
+#     # print(filename.shape)
+#     # print(label.shape)
+#     # image_decoded = cv2.imread(filename)
+#     # depth_decoded = cv2.imread(label)
+#     print(image_decoded.shape)
+#
+#     ## Data augmentation !!!
+#     # optional: rotate15, rotate60, rotate45, vertical_flip, horizontal_flip, random_crop, affine_transfrom
+#     image_new, depth_new = transform.rotate60(np.array(image_decoded), np.array(depth_decoded))
+#
+#     # image_new = []
+#     # depth_new = []
+#     # for i in range(len(filenames)):
+#     #     image_, depth_ = transform.rotate60(np.array(image_decoded[i]), np.array(depth_decoded[i]))
+#     #     image_new.append(image_)
+#     #     depth_new.append(depth_)
+#     # image_new, depth_new = [transform.rotate60(
+#     #     np.array(image_decoded[i]), np.array(depth_decoded[i])) for i in range(len(filenames))]
+#     # image_new = np.stack(image_new)
+#     # depth_new = np.stack(depth_new)
+#     shape_depth = (240, 320, 1)
+#     depth_resized = tf.image.resize(depth_new, [shape_depth[0], shape_depth[1]])
+#
+#     # Format
+#     rgb = tf.image.convert_image_dtype(image_new, dtype=tf.float32)
+#     depth = tf.image.convert_image_dtype(depth_resized / 255.0, dtype=tf.float32)
+#
+#     # Normalize the depth values (in cm)
+#     depth = 1000 / tf.clip_by_value(depth * 1000, 10, 1000)
+#     return rgb, depth
 
 
 ######################### Prepare Trainning Set ##########################
@@ -64,10 +80,33 @@ nyu2_train = sklearn.utils.shuffle(nyu2_train, random_state=0)
 filenames = [os.path.join(root, i[0]) for i in nyu2_train]
 labels = [os.path.join(root, i[1]) for i in nyu2_train]
 length = len(filenames)
-dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
+
+image_list = []
+depth_list = []
+for i in range(length):
+    image_decoded = cv2.imread(filenames[i])
+    depth_decoded = cv2.imread(labels[i], cv2.IMREAD_GRAYSCALE)
+    image_, depth_ = data_augmentation(np.array(image_decoded), np.array(depth_decoded))
+    image_ = np.resize(image_, [480, 640, 3])
+    depth_ = np.resize(depth_, [240, 320])[:, :, np.newaxis]
+    image_list.append(image_)
+    depth_list.append(depth_)
+
+image_new = np.stack(image_list)
+depth_new = np.stack(depth_list)
+
+# Format
+rgb = tf.image.convert_image_dtype(image_new, dtype=tf.float32)
+depth = tf.image.convert_image_dtype(depth_new / 255.0, dtype=tf.float32)
+
+# Normalize the depth values (in cm)
+depth = 1000 / tf.clip_by_value(depth * 1000, 10, 1000)
+
+# dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
+dataset = tf.data.Dataset.from_tensor_slices((rgb, depth))
 dataset = dataset.shuffle(buffer_size=len(filenames), reshuffle_each_iteration=True)
 dataset = dataset.repeat()
-dataset = dataset.map(map_func=_parse_function, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+# dataset = dataset.map(map_func=_parse_function, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 batch_size = args.batch_size  # batch_size from inputs, default value is 2
 train_generator = dataset.batch(batch_size=batch_size)
 
